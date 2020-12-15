6d33e39333f32c5ea0ceccc4ed784d7046304abc
B. Endres <endres@systopia.de>
2020-12-15 11:29:46 +0100

    added contact type

--- CRM/Volunteers/Upgrader.php
+++ CRM/Volunteers/Upgrader.php
@@ -13,11 +13,42 @@ class CRM_Volunteers_Upgrader extends CRM_Volunteers_Upgrader_Base
      */
     public function install()
     {
+        // add contact type
+        self::createVolunteerContactType();
+
         // install remote event stuff
         $customData = new CRM_Volunteers_CustomData(E::LONG_NAME);
         $customData->syncOptionGroup(E::path('resources/option_group_volunteer_qualifications.json'));
         $customData->syncOptionGroup(E::path('resources/option_group_volunteer_skills.json'));
         //    $customData->syncCustomGroup(E::path('resources/custom_group_remote_registration.json'));
+    }
+
+
+    /**
+     * Get the data of the event contact type,
+     *  and creates it if it doesn't exist yet
+     *
+     * @return array
+     *   contact type data
+     */
+    protected static function createVolunteerContactType() {
+        $volunteer_types = civicrm_api3('ContactType', 'get', [
+            'name'         => 'Stibi_Volunteer',
+            'option.limit' => 0
+        ]);
+        if ($volunteer_types['count'] > 1) {
+            Civi::log()->warning(E::ts("Multiple matching Volunteer contact types found!"));
+        }
+        if ($volunteer_types['count'] == 0) {
+            // create it
+            $new_location = civicrm_api3('ContactType', 'create', [
+                'name'        => 'Stibi_Volunteer',
+                'label'       => E::ts("Volunteer"),
+                'description' => E::ts("Stibi Volunteers"),
+                'image_URL'   => E::url('icons/volunteer_type.png'),
+                'parent_id'   => 3, // 'Organisation'
+            ]);
+        }
     }
 
 
@@ -35,5 +66,16 @@ class CRM_Volunteers_Upgrader extends CRM_Volunteers_Upgrader_Base
         return true;
     }
 
-
+    /**
+     * Example: add contact type
+     *
+     * @return TRUE on success
+     * @throws Exception
+     */
+    public function upgrade_0002()
+    {
+        $this->ctx->log->info('create contact type');
+        self::createVolunteerContactType();
+        return true;
+    }
 }
