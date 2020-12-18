c2552fe2c11a8812a36811dc6ef8d88282aa4bb8
Jo Franz <franz@systopia.de>
2020-12-18 11:30:08 +0100

    use generic extension naming in update script

--- update_pot.sh
+++ update_pot.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 # quick and dirty script to update the .pot file
 l10n_tools="../civi_l10n_tools"
-${l10n_tools}/bin/create-pot-files-extensions.sh com.stiftungbildung.volunteers ./ l10n
+${l10n_tools}/bin/create-pot-files-extensions.sh de.systopia.volunteers ./ l10n
